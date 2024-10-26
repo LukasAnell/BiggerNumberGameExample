6062a4f102c235196875eabb8762c90e6eecb11e
Lukas Anell <lukasmanell@gmail.com>
2024-10-26 13:50:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/biggernumbergame/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.example.biggernumbergame
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.biggernumbergame.databinding.ActivityMainBinding
+import kotlin.math.roundToInt
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    var score: Int = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // ignore this stuff
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.layout_main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        /*
+         * talk about naming convention in layout
+         * --> WHAT_WHERE_WHY Ex. "textView_main_score"
+         */
+
+        // initialize view binding
+        // mention lateinit var binding line at the top of the MainActivity class
+        // mention extra code in build.gradle.kts, make sure it's the right one
+        // because there's two different build.gradle.kts
+        // correct one is within the
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // set the textView with the score
+        binding.textViewMainScore.text = "Score: $score"
+
+        // randomize the numbers on each button separately
+        // use a function for this so you can just call it over and over again
+        // will be called every time you click on a button
+        randomizeButtons()
+
+        // set listeners for buttons
+        // --> will run a block of code whenever a button is pressed
+        // kind of like a method/function
+        setListeners()
+    }
+
+    private fun randomizeButtons() {
+        // remember Math.random() syntax for making ranges
+        // Math.random() * possibilities + minimum
+        binding.buttonMainLeft.text = (Math.random() * 100 + 1).roundToInt().toString()
+        binding.buttonMainRight.text = (Math.random() * 100 + 1).roundToInt().toString()
+
+        // if you want, you can use a while loop to keep assigning a new number to the buttons if they turn out to be the same
+        // while num1 == num2 --> reassign random number
+        // will ensure the two numbers are different, which means the two buttons are guaranteed to have different numbers
+    }
+
+    private fun setListeners() {
+        // binding.{element} notation
+        // choose setOnClickListener without ()
+        binding.buttonMainLeft.setOnClickListener {
+            // extract the numbers each button stores and assign them to their own variable
+            // using Google is important, in this case to figure out
+            val button1Val = Integer.parseInt(binding.buttonMainLeft.text.toString())
+            val button2Val = Integer.parseInt(binding.buttonMainRight.text.toString())
+
+            // check if their answer is correct
+            // compare the number stored in the button to the number stored in the other button
+            // if button1 is the bigger number, add to the score
+            // if not, subtract from the score
+            if(button1Val > button2Val) {
+                score++
+            } else {
+                // if you want, you can make sure the score doesn't go below 0
+                // if(score < 0) --> do nothing
+                // else --> reduce the score
+                score--
+            }
+
+            // update textView with the score
+            binding.textViewMainScore.text = "Score: $score"
+
+            // re-randomize buttons
+            randomizeButtons()
+        }
+
+        binding.buttonMainRight.setOnClickListener {
+            // do the same thing but the other way
+            // so if button2 is correct you get points
+            val button1Val = Integer.parseInt(binding.buttonMainLeft.text.toString())
+            val button2Val = Integer.parseInt(binding.buttonMainRight.text.toString())
+
+            if(button2Val > button1Val) {
+                score++
+            } else {
+                score--
+            }
+
+            binding.textViewMainScore.text = "Score: $score"
+
+            randomizeButtons()
+        }
+    }
+}
